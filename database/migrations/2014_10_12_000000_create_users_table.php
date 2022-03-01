c82d0ca007d74f67bc47c1f73212d92d9a49f171
unknown <fandiaja101@gmail.com>
2022-03-01 15:59:08 +0700

    aplikasi kasir

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('level');
+            $table->string('username');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        
+        // Schema::table('users', function (Blueprint $table) {        
+        //     $table->foreign('name')->references('nama_pegawai')->on('transaction');
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
