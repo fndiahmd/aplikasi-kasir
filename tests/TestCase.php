732555a50de88f8d9b31c3dc25e33d30a5055d8d
Fandi Ahmad <73821279+fndiahmd@users.noreply.github.com>
2023-03-25 06:38:59 +0700

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,3 +8,16 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 }
+
+
+// <?php
+// 	if(!empty($this->session->flashdata('succeed'))){
+// 		$succeed = '<div class="alert dark alert-alt alert-success alert-dismissible" role="alert">
+// 					  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+// 					    <span aria-hidden="true"><i class="icon wb-close-mini" aria-hidden="true"></i></span>
+// 					  </button>
+// 					  '.$this->session->flashdata('succeed').'
+// 					</div>';
+// 		echo $succeed;
+// 	}
+// ?>
